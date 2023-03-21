3aa9ee688a2f9ca9baf8ffe1c67a01857003e2ad
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-03-22 05:24:40 +0600

    SqlKata project create

---
+++ DynamicTableService.SqlKataQuerier/DTManagerSqlKata.cs
@@ -0,0 +1,19 @@
+﻿using DynamicTableService.Components;
+using SqlKata;
+
+namespace DynamicTableService.SqlKataQuerier
+{
+    public class DTManagerSqlKata : DTManagerNoQuerier
+    {
+        public readonly DTQuerier Querier;
+
+        public DTManagerSqlKata(string connectionString) : base(connectionString)
+        {
+            Querier = new DTQuerier(connectionString);
+        }
+
+        public DTManagerSqlKata() : this(getConnectionStringFromConfig())
+        {
+        }
+    }
+}
