abefabb1629fe1198d066325f48945a3a52dcf53
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-03-22 04:27:01 +0600

    adding functional

--- ConsoleDTApp/TableConsole.cs
+++ ConsoleDTApp/TableConsole.cs
@@ -1,4 +1,5 @@
 ﻿using DynamicTableService;
+using System.ComponentModel;
 
 namespace ConsoleDTApp
 {
@@ -9,10 +10,13 @@ namespace ConsoleDTApp
         private ConsoleView view = new();
 
         private enum todoMain
-        { ChooseTable, CreateTable, EditTable, ShowTablesList };
+        { ChooseTable, CreateTable, EditTable, ShowTablesList, QueryTable };
 
         private enum todoEditTable
         { RenameTable, AddColumn, EditColumn, RenameColumn, AddPrimaryKey, AddPriamryKeys, DropTable, DropColumn, DropPrimaryKeys };
+
+        private enum todoQueryTable
+        { ShowTable, AddRow };
 
         public TableConsole()
         {
@@ -34,7 +38,9 @@ namespace ConsoleDTApp
                     break;
 
                 case todoMain.ChooseTable:
-                    view.printMsg(chooseTable() ? $"Chosen table is '{chosenTable}'" : "No table chosen");
+                    view.printMsg(chooseTable()
+                        ? $"Chosen table is '{chosenTable}':\n{string.Join(" | ", dtManager.Scaner.getColsKeysAndTypes(chosenTable))}"
+                        : "No table chosen");
                     break;
 
                 case todoMain.ShowTablesList:
@@ -43,6 +49,10 @@ namespace ConsoleDTApp
 
                 case todoMain.EditTable:
                     editTable();
+                    break;
+
+                case todoMain.QueryTable:
+                    queryTable();
                     break;
 
                 default:
@@ -56,7 +66,7 @@ namespace ConsoleDTApp
         {
             var tabnames = dtManager.Scaner.getTablesNames();
             view.printMsg(tabnames.Count > 0
-                ? $"Here is the list of tables:\n {string.Join(",", tabnames)}"
+                ? $"Here is the list of tables:\n {string.Join(", ", tabnames)}"
                 : "No tables in Db");
         }
 
@@ -118,6 +128,99 @@ namespace ConsoleDTApp
             nullable = view.getChoice(new[] { "Null", "Not Null" }, "Choose if nullable:") != "Not Null";
 
             dtManager.Builder.AddColumn(chosenTable, colname, type, len, nullable);
+        }
+
+        public void queryTable()
+        {
+            bool tableCosen = chooseTable();
+            if (!tableCosen) return;
+
+            var keysAndTypes = dtManager.Scaner.getColsKeysAndTypes(chosenTable);
+            view.printMsg($"'{chosenTable}' table keys and types:\n{string.Join(" | ", keysAndTypes)}");
+
+            bool resume = true;
+            while (resume)
+            {
+                resume = chooseToDoQueryTable();
+            }
+        }
+
+        public bool chooseToDoQueryTable()
+        {
+            var todoQueryTableRes = view.getChoiceEnum<todoQueryTable>("Choose what to do");
+            if (todoQueryTableRes == null) return false;
+            switch (todoQueryTableRes)
+            {
+                case todoQueryTable.ShowTable:
+                    showTable();
+                    break;
+                case todoQueryTable.AddRow:
+                    view.printMsg(addRow() ? "Row added" : "Row not added");
+                    break;
+                default:
+                    view.printMsg("Nothing chosen to do");
+                    return false;
+            }
+            return true;
+        }
+
+        public void showTable()
+        {
+            dtManager.Querier.setTable(chosenTable);
+            var table = dtManager.Querier.Select();
+
+            string tableStr = $"Table '{chosenTable}'\n";
+            table.ForEach(row =>
+            {
+                tableStr += string.Join(" | ", row) + "\n";
+            });
+            view.printMsg(tableStr);
+        }
+
+        public bool addRow()
+        {
+            dtManager.Querier.setTable(chosenTable);
+            Dictionary<string, object> row = new();
+            var colsKeysAndTypes = dtManager.Scaner.getColsKeysAndCSTypes(chosenTable);
+            foreach (var col in colsKeysAndTypes)
+            {
+            // label
+            repeat:
+                var type = col.Value;
+                var input = view.getStringname($"Enter {type.Name} value for {col.Key} column:");
+                var converter = TypeDescriptor.GetConverter(type);
+                if (converter != null && input != null && input != "")
+                {
+                    var convres = converter.ConvertFromString(input);
+                    if (convres != null)
+                    {
+                        row[col.Key] = convres;
+                        continue;
+                    }
+                }
+                var choiceRes = view.getChoice(new[] { "No value", "Null", "Empty str", "Rechoose" }, $"No value. Chose what to set as {col.Key}:");
+                switch (choiceRes)
+                {
+                    case "No value":
+                        continue;
+                    case "Null":
+                        row[col.Key] = null;
+                        break;
+                    case "Empty str":
+                        row[col.Key] = "";
+                        break;
+                    case "Rechoose":
+                        goto repeat;
+                }
+            }
+
+            view.printMsg("Row:\n" + string.Join(" | ", row));
+            if (view.getChoice(new[] { "Insert row", "Dont insert row" }) == "Insert row")
+            {
+                dtManager.Querier.Insert(row);
+                return true;
+            }
+            return false;
         }
 
         public bool chooseTable()
