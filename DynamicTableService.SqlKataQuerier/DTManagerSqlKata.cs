317bcb06e9c85abc465bf59dec6e06e5e28bf9fa
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-03-22 05:46:50 +0600

    Setup SqlKata querier

--- DynamicTableService.SqlKataQuerier/DTManagerSqlKata.cs
+++ DynamicTableService.SqlKataQuerier/DTManagerSqlKata.cs
@@ -1,15 +1,19 @@
-﻿using DynamicTableService.Components;
-using SqlKata;
+﻿using SqlKata.Compilers;
+using SqlKata.Execution;
+using System.Data.SqlClient;
 
 namespace DynamicTableService.SqlKataQuerier
 {
     public class DTManagerSqlKata : DTManagerNoQuerier
     {
-        public readonly DTQuerier Querier;
+        public readonly QueryFactory Querier;
 
         public DTManagerSqlKata(string connectionString) : base(connectionString)
         {
-            Querier = new DTQuerier(connectionString);
+            SqlConnection connection = new SqlConnection(connectionString);
+            Compiler compiler = new SqlServerCompiler();
+
+            Querier = new QueryFactory(connection, compiler);
         }
 
         public DTManagerSqlKata() : this(getConnectionStringFromConfig())
