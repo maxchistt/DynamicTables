34411f0d68fbcf2b5cdc283f905b0fa549d0e95e
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-03-26 20:07:28 +0600

    condition in

--- DynamicTableService/WhereCondition.cs
+++ DynamicTableService/WhereCondition.cs
@@ -1,7 +1,8 @@
-﻿namespace DynamicTableService.Components
+﻿using System.Collections;
+namespace DynamicTableService
 {
     public enum ConditionOperator
-    { Equal, GreaterThan, LessThan, GreaterThanOrEqual, LessThanOrEqual, NotEqual }
+    { Equal, GreaterThan, LessThan, GreaterThanOrEqual, LessThanOrEqual, NotEqual, In }
 
     public struct WhereCondition
     {
@@ -12,6 +13,7 @@
             { ConditionOperator.GreaterThanOrEqual, ">="},
             { ConditionOperator.LessThanOrEqual, "<="},
             { ConditionOperator.NotEqual, "!="},
+            { ConditionOperator.In, "IN"},
         };
 
         private string _column = "col1";
@@ -32,14 +34,57 @@
             _operator = ConditionOperatorsStrings.FirstOrDefault(v => v.Value == conditionOperator).Key;
         }
 
-        private static string OperatorToString(ConditionOperator _operator)
+        private string OperatorToString()
         {
             return ConditionOperatorsStrings[_operator];
         }
 
+        private List<object>? ValueToObjectList()
+        {
+            var list = new List<object>();
+
+            if (_value is IEnumerable enumerable)
+            {
+                foreach (var val in enumerable)
+                {
+                    list.Add(val);
+                }
+            }
+            else if (_value is System.Runtime.CompilerServices.ITuple tuple)
+            {
+                for (int i = 0; i < tuple.Length; i++)
+                {
+                    list.Add(tuple[i]);
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            return list;
+        }
+
+        private string ValueToString()
+        {
+            string resStr = string.Empty;
+            switch (_operator)
+            {
+                case ConditionOperator.In:
+                    var list = ValueToObjectList();
+                    if (list == null) throw new ArgumentException("Value should be enumerable or tuple if condition needs few values", "object value");
+                    resStr = $"({string.Join(", ", list.Select(Components.QueryBuilder.GetValueString))})";
+                    break;
+                default:
+                    resStr = Components.QueryBuilder.GetValueString(_value);
+                    break;
+            }
+            return resStr;
+        }
+
         public new string ToString()
         {
-            return $"{_column} {OperatorToString(_operator)} {Components.QueryBuilder.GetValueString(_value)}";
+            return $"{_column} {OperatorToString()} {ValueToString()}";
         }
     }
 }
