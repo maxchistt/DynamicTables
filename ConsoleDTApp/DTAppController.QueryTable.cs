63aeae52fb81590a65d9c23004f806d69f900ba3
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-03-27 00:44:00 +0600

    where test in console app

--- ConsoleDTApp/DTAppController.QueryTable.cs
+++ ConsoleDTApp/DTAppController.QueryTable.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel;
+﻿using DynamicTableService;
+using System.ComponentModel;
 
 namespace ConsoleDTApp
 {
     internal partial class DTAppController
     {
         private enum todoQueryTable
-        { ShowTable, AddRow };
+        { ShowTable, AddRow, SelectWithParams };
 
         public void queryTable()
         {
@@ -36,11 +37,148 @@ namespace ConsoleDTApp
                     view.printMsg(addRow() ? "Row added" : "Row not added");
                     break;
 
+                case todoQueryTable.SelectWithParams:
+                    selectAndShow();
+                    break;
+
                 default:
                     view.printMsg("Nothing chosen to do");
                     return false;
             }
             return true;
+        }
+
+        public void selectAndShow()
+        {
+            string tableName = chosenTable;
+
+            ///////// columnNames
+
+            List<string>? columnNames = null;
+            string chosenField = "*";
+            while (chosenField != "")
+            {
+                chosenField = view.getChoice(dtManager.Scaner.getColsKeys(chosenTable).ToArray(), "Chose fields to select. Press enter to end/skip selection.");
+                if (chosenField != "")
+                {
+                    if (columnNames == null) columnNames = new();
+                    columnNames.Add(chosenField);
+                    view.printMsg(string.Join(" & ", columnNames));
+                }
+            }
+
+            ///////// whereConditions
+
+            List<WhereCondition>? whereConditions = null;
+            bool breakWhereConditions = false;
+            while (!breakWhereConditions)
+            {
+                if (view.getChoice(new string[] { "Yes", "No" }, "Add another where condition?") != "Yes")
+                {
+                    breakWhereConditions = true;
+                    break;
+                }
+
+                string column = "";
+                while (column == "")
+                {
+                    column = view.getChoice(dtManager.Scaner.getColsKeys(chosenTable).ToArray(), "Chose field for where condition");
+                    if (column == "")
+                    {
+                        breakWhereConditions = true;
+                        break;
+                    }
+                }
+                if (breakWhereConditions) break;
+
+                ///
+
+                ConditionOperator conditionOperator;
+                ConditionOperator? op = null;
+                while (op == null)
+                {
+                    op = view.getChoiceEnum<ConditionOperator>("Chose operator for where condition");
+                    if (op == null)
+                    {
+                        breakWhereConditions = true;
+                        break;
+                    }
+                    conditionOperator = op ?? ConditionOperator.Equal;
+                }
+                if (breakWhereConditions) break;
+
+                ///
+
+                List<object> valuesList = new();
+                bool repeatFieldsChoose = true;
+                while (repeatFieldsChoose)
+                {
+                    bool enterStr = (view.getChoice(new string[] { "Number", "String" }, "What type whould you enter?") != "Number");
+
+                    string? valueStr = view.getStringname("Enter value")?.Trim();
+                    if (valueStr == null || valueStr == "")
+                    {
+                        repeatFieldsChoose = false;
+                        breakWhereConditions = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (enterStr)
+                        {
+                            valuesList.Add(valueStr);
+                        }
+                        else
+                        {
+                            double valueDoubleParsed;
+                            if (double.TryParse(valueStr, out valueDoubleParsed))
+                            {
+                                valuesList.Add(valueDoubleParsed);
+                            }
+                            else
+                            {
+                                view.printMsg("numeric value wasnt parsed");
+                            }
+                        }
+
+                        view.printMsg(string.Join(" & ", valuesList));
+                    }
+
+                    if (view.getChoice(new string[] { "Yes", "No" }, "Add another value for this condition?") != "Yes") repeatFieldsChoose = false;
+                }
+                if (breakWhereConditions) break;
+
+                ///
+
+                WhereCondition cond = new WhereCondition(column, conditionOperator, valuesList);
+
+                if (whereConditions == null) whereConditions = new();
+                whereConditions.Add(cond);
+            }
+
+            ///////// groupByColumnName
+
+            string? groupByColumnName = null;
+
+            ///////// orderByColumnName orderByDescend
+
+            string? orderByColumnName = null;
+            bool? orderByDescend = null;
+
+            ///////// offsetAndFetch
+
+            Tuple<int, int>? offsetAndFetch = null;
+
+            /////////
+
+            var table = dtManager.Querier.Select(tableName, columnNames, whereConditions, groupByColumnName, orderByColumnName, orderByDescend, offsetAndFetch);
+
+            string tableStr = $"Table '{chosenTable}'\n";
+            table.ForEach(row =>
+            {
+                tableStr += string.Join(" | ", row) + "\n";
+            });
+            view.printMsg(tableStr);
         }
 
         public void showTable()
